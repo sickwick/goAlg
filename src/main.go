f4902ac49935d53ec90c13cc6a8ae3dc005dc10f
Mihail Shcherbakov <mihail.shch@gmail.com>
2020-09-15 22:39:01 +0300

    Add timer

--- src/main.go
+++ src/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"algorithms"
 	"fmt"
+	"time"
 )
 
 func main() {
 	arr := []int{4, 12, 3, 2, 23, 2, 5, 0, 90, 8, 4, 65}
-	//fmt.Println(algorithms.BubbleSort(arr))
-	fmt.Println(algorithms.QuickSort(arr))
+	start := time.Now()
+	fmt.Println(algorithms.BubbleSort(arr))
+	//fmt.Println(algorithms.QuickSort(arr))
+	fmt.Println(time.Since(start).Microseconds())
 }
